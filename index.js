3f61f1a5bb92d8e5869a4e8b8b15c26454acb45a
unknown <ksemenza13@gmail.com>
2020-03-11 16:14:03 -0400

    mvp

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+
+const express = require('express');
+const server = require('./server.js')
+
+
+const port = process.env.PORT || 9000;
+server.listen(port, () => console.log(`API RUNNING PORT: ${port}`))
