4970b1fd52a38accd6d9bb57464ffb502148c8a7
unknown <ksemenza13@gmail.com>
2020-03-12 09:22:24 -0400

    updated acess

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 const express = require('express');
 const server = require('./server.js')
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 
 
 const port = process.env.PORT || 9000;
